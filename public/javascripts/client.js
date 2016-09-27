0cb267277f7592003169a02d53fab94b769d15c8
Iggi <iggirex@gmail.com>
2016-09-26 23:04:46 -0600

    initial commit

---
+++ public/javascripts/client.js
@@ -0,0 +1,22 @@
+console.log("WE are in CLIENT")
+var socket = io();
+
+var webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remoteVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true
+});
+
+webrtc.on('connection', function () {
+    // you can name it anything
+    webrtc.joinRoom('your awesome room name');
+});
+
+// we have to wait until it's ready
+webrtc.on('readyToCall', function () {
+    // you can name it anything
+    webrtc.joinRoom('your awesome room name');
+});
---
+++ sockets.js
@@ -0,0 +1,13 @@
+var io = require("socket.io")()
+
+io.on('connection', function (socket)  {
+  console.log('WE are insinde connection')
+  socket.on('peer-msg', function (data) {
+    console.log('Message from peer: %s', data);
+    socket.broadcast.emit('peer-msg', data);
+  })
+
+  socket.on('go-private', function (data) {
+    socket.broadcast.emit('go-private', data);
+  });
+});
